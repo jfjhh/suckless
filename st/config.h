32010f7dcf4d136415785441f56639405219be1a
jfjhh <alex.striff1@gmail.com>
2015-05-16 21:22:21 -0700

    Executables removed from repo. st font size now 16

--- st/config.h
+++ st/config.h
@@ -5,7 +5,8 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char font[] = "Anonymous Pro:pixelsize=14:antialias=true:autohint=true";
+/* static char font[] = "Anonymous Pro:pixelsize=14:antialias=true:autohint=true"; */
+static char font[] = "Anonymous Pro:pixelsize=16:antialias=true:autohint=true";
 static int borderpx = 2;
 static char shell[] = "/bin/bash";
 static char *utmp = NULL;
