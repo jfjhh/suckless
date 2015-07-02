9691cadcf1ad9ae114e9179d9b3d9c232cbf294b
jfjhh <alex.striff1@gmail.com>
2015-07-02 00:00:08 -0700

    auto push_repos (07-02-15/00:00:08).

--- st/config.h
+++ st/config.h
@@ -45,7 +45,7 @@ static unsigned int blinktimeout = 800;
  * bell volume. It must be a value between -100 and 100. Use 0 for disabling
  * it
  */
-static int bellvolume = 0;
+static int bellvolume = 100;
 
 /* TERM value */
 static char termname[] = "xterm-256color";
