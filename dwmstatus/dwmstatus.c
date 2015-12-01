69152fdd6ca277c39c19986924ec9884d7e87dcc
jfjhh <alex.striff1@gmail.com>
2015-11-30 20:53:33 -0800

    Added cpu usage monitor

--- dwmstatus/dwmstatus.c
+++ dwmstatus/dwmstatus.c
@@ -1,5 +1,15 @@
 /* A status bar for dwm. */
 
+/* Feature-test macro for nanosleep(2). */
+#if ! (_POSIX_C_SOURCE >= 199309L)
+#define _POSIX_C_SOURCE 199309L
+#endif /* _POSIX_C_SOURCE */
+
+/* Feature-test macro for snprintf(3). */
+#if _BSD_SOURCE
+#define _BSD_SOURCE
+#endif /* _BSD_SOURCE */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -9,101 +19,22 @@
 #include <time.h>
 #include <X11/Xlib.h>
 
+/* Buffer size for status string. */
+const static size_t  STATUS_LEN = 256;
+
+/* Percentage to display if an error occurs in reading cpu usage. */
+const static float  ERROR_CPU_PER = 3133.7;
+
+/* MONITOR_SLEEP_INTERVAL must be a multiple of STATUS_SLEEP_INTERVAL. */
+const static struct timespec STATUS_SLEEP_INTERVAL  = { (time_t) 1, 0L };
+const static struct timespec MONITOR_SLEEP_INTERVAL = { (time_t) 4, 0L };
+
 static Display *dpy;
 
 void setstatus(char *str)
 {
 	XStoreName(dpy, DefaultRootWindow(dpy), str);
 	XSync(dpy, False);
-}
-
-float getfreq(char *file)
-{
-	FILE *fd;
-	char *freq;
-	float ret;
-
-	freq = malloc(10);
-	fd = fopen(file, "r");
-	if(fd == NULL) {
-		fprintf(stderr, "Cannot open '%s' for reading.\n", file);
-		exit(1);
-	}
-
-	fgets(freq, 10, fd);
-	fclose(fd);
-
-	ret = atof(freq) / 1e6;
-	free(freq);
-	return ret;
-}
-
-char *getcpu(void)
-{
-	FILE *fd;
-	char *buf, *buf_part, *scaling_cur_freq;
-	size_t buf_len, buf_part_len, scaling_cur_freq_len;
-	int min, max, cur;
-	float *freqs;
-
-	fd = fopen("/sys/devices/system/cpu/online", "r");
-	if (fd == NULL) {
-		fprintf(stderr, "Error getting the number of cpus online.\n");
-		exit(1);
-	}
-
-	fscanf(fd, "%d-%d", &min, &max);
-	fclose(fd);
-
-	if (max >= 100) {
-		fprintf(stderr, "%d cores!? Too many!\n", max);
-		exit(1);
-	}
-
-	if ((freqs = (float *) malloc(sizeof(float) * (max + 1))) == NULL) {
-		fprintf(stderr, "Cannot allocate memory for cpu frequencies.\n");
-		exit(1);
-	}
-
-	// 6 chars for each core, plus the terminating '\0' byte.
-	buf_part_len = 7;
-	if ((buf_part = (char *) malloc(sizeof(char) * buf_part_len)) == NULL) {
-		fprintf(stderr, "Cannot allocate memory for buf_part.\n");
-		exit(1);
-	}
-	memset(buf_part, '\0', buf_part_len);
-
-	// buf_part_len chars for each core; take 2 chars to not have ", " at end.
-	buf_len = (((max + 1) * (buf_part_len - 1)) - 2 ) + 1;
-	if ((buf = (char *) malloc(sizeof(char) * buf_len)) == NULL) {
-		fprintf(stderr, "Cannot allocate memory for buf.\n");
-		exit(1);
-	}
-	memset(buf, '\0', buf_len);
-
-	for (cur = min; cur <= max; cur++) {
-		scaling_cur_freq_len =
-			strlen("/sys/devices/system/cpu/cpu/cpufreq/scaling_cur_freq") +
-			1 + ((cur >= 10) ? 2 : 1);
-		scaling_cur_freq = (char *) malloc(sizeof(char) * scaling_cur_freq_len);
-
-		snprintf(scaling_cur_freq, scaling_cur_freq_len,
-				"/sys/devices/system/cpu/cpu%d/cpufreq/scaling_cur_freq", cur);
-
-		freqs[cur] = getfreq(scaling_cur_freq);
-
-		if (cur != max)
-			snprintf(buf_part, buf_part_len, "%0.2f, ", freqs[cur]);
-		else
-			snprintf(buf_part, buf_part_len - 2, "%0.2f", freqs[cur]);
-
-		strncat(buf, buf_part, buf_len + buf_part_len - 1);
-		free(scaling_cur_freq);
-	}
-
-	free(freqs);
-	free(buf_part);
-	return buf;
 }
 
 char *getdatetime(void)
@@ -208,57 +139,114 @@ float ramusage(void)
 	return ((float) active / (float) total) * 100.0;
 }
 
-float cpuusage(void)
+float cpuusage()
 {
 	FILE *fd;
-	int user_jiff, nice_jiff, sys_jiff;
+	int user_jiff, nice_jiff, sys_jiff, idle_jiff, new_user_jiff,
+		new_nice_jiff, new_sys_jiff, new_idle_jiff;
+
+	/* Example line from `/proc/stat`:
+	 * cpu  234646 66 57678 21381296 43506 2 362 0 0 0
+	 */
 
 	if (!(fd = fopen("/proc/stat", "r"))) {
 		fprintf(stderr, "Error opening /proc/stat.\n");
-		return -1;
+		return ERROR_CPU_PER;
 	}
+	if (fscanf(fd, "cpu  %d %d %d %d", &user_jiff, &nice_jiff,
+				&sys_jiff, &idle_jiff) != 4)
+		return ERROR_CPU_PER;
 
-	// cpu  234646 66 57678 21381296 43506 2 362 0 0 0
-	fscanf(fd, "cpu  %d %d %d", &user_jiff, &nice_jiff, &sys_jiff);
+	nanosleep(&MONITOR_SLEEP_INTERVAL, NULL);
+
+	if (!(fd = freopen("/proc/stat", "r", fd))) {
+		fprintf(stderr, "Error opening /proc/stat.\n");
+		return 7331.3;
+	}
+	if (fscanf(fd, "cpu  %d %d %d %d", &new_user_jiff, &new_nice_jiff,
+				&new_sys_jiff, &new_idle_jiff) != 4)
+		return 7331.3;
+
+	fclose(fd);
+
+	/* Calculate percentage from diffs in jiffies between work and idle. */
+	return ((float) ((user_jiff + nice_jiff + sys_jiff)
+				- (new_user_jiff + new_nice_jiff + new_sys_jiff))
+			/ (float) (idle_jiff - new_idle_jiff)) * 100.0;
 }
 
 int main(void)
 {
-	char *status, *datetime, *cpu_str;
-	float bat, ram, cpu;
+	char *status, *datetime;
+	float bat, ram;
+	int pipefd[2];
+	pid_t pid;
+	union fbuf {
+		float f;
+		void *b;
+	};
 
 	if (!(dpy = XOpenDisplay(NULL))) {
 		fprintf(stderr, "Cannot open display.\n");
 		return 1;
 	}
 
-	if ((status = (char *) malloc(sizeof(char) * 200)) == NULL) {
+	if ((status = (char *) malloc(sizeof(char) * STATUS_LEN)) == NULL) {
 		fprintf(stderr, "Cannot allocate memory for status string.\n");
 		return 1;
 	}
 
-	for (;; sleep(1)) {
-		cpu_str = getcpu();
-		ram = ramusage();
-		cpu = cpuusage();
-		datetime = getdatetime();
-		bat = getbattery();
+	if (pipe(pipefd) == -1) {
+		perror("cpu monitor pipe");
+		exit(1);
+	}
+	pid = fork();
 
-		// if (bat != -1.0)
-		// 	snprintf(status, 200, "[ %s | %0.1f%% ]  [ %0.1f%% | %s ]",
-		// 			cpu_str, ram, bat, datetime);
-		// else
-		// 	snprintf(status, 200, "[ %s | %0.1f%% ]  [ %s ]",
-		// 			cpu_str, ram, datetime);
+	/* Fork off CPU usage monitor. */
+	if (pid == -1) { /* error */
+		fputs("Failed to fork cpu percentage monitor.\n", stderr);
+		exit(1);
+	} else if (pid == 0) { /* child */
+		close(pipefd[0]);
+
+		union fbuf cbuf = { 0 };
+		for (;;) { /* forever */
+			cbuf.f = cpuusage();
+
+			if (write(pipefd[1], &(cbuf.b), sizeof(cbuf.f)) == -1) {
+				perror("cpu monitor write");
+				exit(1);
+			}
+		}
+	} else { /* parent */
+		close(pipefd[1]);
+	}
+
+	union fbuf cbuf = { 0 };
+	for (time_t status_sleeps = (time_t) 0 ;; status_sleeps++,
+			nanosleep(&STATUS_SLEEP_INTERVAL, NULL)) {
+		ram      = ramusage();
+		datetime = getdatetime();
+		bat      = getbattery();
+
+		if (status_sleeps == MONITOR_SLEEP_INTERVAL.tv_sec
+				/ STATUS_SLEEP_INTERVAL.tv_sec) {
+			if (read(pipefd[0], &(cbuf.b), sizeof(cbuf.f)) == -1) {
+				perror("cpu monitor read");
+				exit(1);
+			}
+			status_sleeps = 0;
+		}
 
 		if (bat != -1.0)
-			snprintf(status, 200, "[ %0.1f%% | %0.1f%% ]  [ %0.1f%% | %s ]",
-					cpu, ram, bat, datetime);
+			snprintf(status, STATUS_LEN,
+					"[ %.1f%% cpu | %.1f%% mem ]  [ %.1f%% | %s ]",
+					cbuf.f, ram, bat, datetime);
 		else
-			snprintf(status, 200, "[ %0.1f%% | %0.1f%% ]  [ %s ]",
-					cpu, ram, datetime);
+			snprintf(status, STATUS_LEN,
+					"[ %.1f%% cpu | %.1f%% mem ]  [ %s ]",
+					cbuf.f, ram, datetime);
 
-		free(cpu_str);
 		free(datetime);
 		setstatus(status);
 	}
